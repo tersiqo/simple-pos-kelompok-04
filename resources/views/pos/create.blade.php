b5a118abb5832e78f332480bd542c4189418027b
tugas <aldayanuar6@gmail.com>
2026-09-17 21:59:25 +0700

    cashier-page

--- resources/views/pos/create.blade.php
+++ resources/views/pos/create.blade.php
@@ -6,7 +6,7 @@
         @foreach ($products as $product)
             <div class="border rounded-md p-3">
                 <p class="font-medium">{{ $product->name }}</p>
-                <p class="text-sm text-slate-500">Rp {{ number_format($product->price, 0, ',', '.') }}</p>
+                <p class="text-sm text-slate-500">Rp  {{ number_format($product->price)}}</p>
             </div>
             @endforeach
         </div>
